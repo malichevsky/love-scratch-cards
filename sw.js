00e74ac87efc0f65305077ebc65d1cebb604583c
malichevsky <drogulpetr145@gmail.com>
2026-02-28 14:38:08 +0200

    Fix ServiceWorker paths and registration for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'love-scratch-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 // Install the service worker
